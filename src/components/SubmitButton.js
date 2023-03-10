a90b4dc62ca01629e14a5a0bc06287d9dd395375
Eemeli <eemeli.oksanen@aalto.fi>
2023-03-10 23:36:56 +0200

    change "submit" to "Valmis!"

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -6,7 +6,7 @@ const SubmitButton = ({ onSubmit }) => {
         className='text-white pl-10 pr-10 border border-green-300 mt-10 rounded-lg bg-green-500 h-10 text-center'
         onClick={onSubmit}
       >
-        submit
+        Valmis!
       </button>
     </div>
   )
